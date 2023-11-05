f30b777895b2577c8ef9b857dc8ea2c3b3bbc996
Kaitlin Nguyen <cookykn@gmail.com>
2023-11-04 22:10:45 -0700

    deleted a comment and also testing repo

--- Character.gd
+++ Character.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var _animated_sprite = $TestCharacterSprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
