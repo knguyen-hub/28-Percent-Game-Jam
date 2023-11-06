806875e3d3a757e228e494764e859db4ce6c0d79
Kaitlin Nguyen <cookykn@gmail.com>
2023-11-05 22:16:13 -0800

    sobbing crying also added animation for left right and down

--- Character.gd
+++ Character.gd
@@ -8,9 +8,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_animated_sprite.play("run")
-	if Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("run")
+	#_animated_sprite.play("run")
+	if Input.is_action_pressed("ui_down"):
+		_animated_sprite.play("down")
+		
+	elif Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("sideways")
+		_animated_sprite.flip_h = false
+		
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("sideways")
+		_animated_sprite.flip_h = true
 	else:
 		_animated_sprite.play("idle")
 #hellooo :)) 
