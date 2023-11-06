12712434a4ee9c3013d4636eb3d54fcdca154a09
Kaitlin Nguyen <cookykn@gmail.com>
2023-11-06 12:17:44 -0800

    added movement and animation for up

--- Character.gd
+++ Character.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $TestCharacterSprite
+var speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass 
@@ -11,16 +12,28 @@ func _process(delta):
 	#_animated_sprite.play("run")
 	if Input.is_action_pressed("ui_down"):
 		_animated_sprite.play("down")
-		
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		_animated_sprite.play("up")
+		velocity.y = - speed
+		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
 		_animated_sprite.play("sideways")
 		_animated_sprite.flip_h = false
-		
+		velocity.x = speed
+		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		_animated_sprite.play("sideways")
 		_animated_sprite.flip_h = true
+		velocity.x = - speed
+		velocity.y = 0
 	else:
+		velocity.x = 0
+		velocity.y = 0
 		_animated_sprite.play("idle")
+		
+	move_and_slide()
 #hellooo :)) 
 
 #PLEASE WORK t-t
