713fa85e56a1248b8b2e7d7af170e6b7f7a4469a
Elaine Hoang <elahoang@uw.edu>
2023-11-10 15:00:15 -0800

    ah

--- Mirror Scene.gd
+++ Mirror Scene.gd
@@ -1,11 +1,6 @@
 extends TextureRect
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/mirror_scene.dialogue"), "start")
+		return
