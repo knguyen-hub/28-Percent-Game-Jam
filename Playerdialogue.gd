a4760a2fa68c8217eae68382935b7ecdd2b9e5e7
reann <reann.corrine05@gmail.com>
2023-11-09 16:37:30 -0800

    added dialogue changes to yur mom

---
+++ Playerdialogue.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		DialogueManager.show_example_dialogue_balloon(load("res://test/visual_novel/test.dialogue"), "start")
+		return
